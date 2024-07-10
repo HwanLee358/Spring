a0e81dd139cbd786b01082940801a8c85546c248
HwanLee358 <clse0286@naver.com>
2024-07-10 16:23:27 +0900

    Merge branch 'main' of https://github.com/HwanLee358/Spring.git into main

--- app/src/main/java/com/yedam/spring/annotation/Restaurant.java
+++ app/src/main/java/com/yedam/spring/annotation/Restaurant.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 public class Restaurant {
 	private Chef chef;
 	
+	@Autowired
 	Restaurant(Chef chef) {
 		System.out.println("생성자 인젝션");
 		this.chef = chef;
