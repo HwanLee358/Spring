022eacf8c1637e20c284311c89e5ccc9264188f6
HwanLee358 <clse0286@naver.com>
2024-07-29 00:09:55 +0900

    init

--- webSoketTest/src/main/java/com/yedam/app/board/service/ChatMessage.java
+++ webSoketTest/src/main/java/com/yedam/app/board/service/ChatMessage.java
@@ -1,20 +1,8 @@
 package com.yedam.app.board.service;
 
+import lombok.Data;
+
+@Data
 public class ChatMessage {
 	private String content;
-
-    public ChatMessage() {
-    }
-
-    public ChatMessage(String content) {
-        this.content = content;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
